57e12715e4835f80df64ed0f9b597e7f6ce63744
binchengecon <bin.cheng@tse-fr.eu>
2023-04-13 23:08:22 -0500

    new

--- graph.py
+++ graph.py
@@ -81,6 +81,50 @@ Moll_Vacenter = np.array(Moll_Vacenter)
 Moll_Va = pd.read_csv("./MollData/Va_Upwind.csv", header = None)
 Moll_Va = np.array(Moll_Va)
 
+Moll_Va_NN = pd.read_csv("./MollData/MollProblem_Va.csv", header=None,index_col=None)
+Moll_Va_NN = np.array(Moll_Va_NN)
+Moll_Va_NN = Moll_Va_NN[1:,1:]
+plt.style.use('classic')
+plt.rcParams["savefig.bbox"] = "tight"
+# plt.rcParams["figure.figsize"] = (10,8)
+# plt.rcParams["figure.dpi"] = 500
+plt.rcParams["font.size"] = 16
+plt.rcParams['text.usetex']=True
+# plt.rcParams["legend.frameon"] = True
+# plt.rcParams["lines.linewidth"] = 5
+
+# fig = plt.figure(figsize=(16, 9))
+# plt.plot(Z[:, 0], Moll_Vacenter[:, 0], label=r'$\partial_a^{Forward} v( \underline a,z)$', color='black')
+# # plt.plot(Z[:,0], Moll_Va[:,0],label='$\partial_a v(a,z)$: Upwind')
+# plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
+#          label=r'$u^\prime(z + r \underline a )$', color='red')
+# # plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
+# #          label=r'$u^\prime(z + r a)$', color = 'red')
+# # plt.view_init(35, 35)
+# plt.xlabel('$z$')
+# plt.ylim(0.75, 1.10)
+# plt.legend()
+# # plt.show()
+# # ax.set_zlabel('$\partial V / \partial a$')
+# # ax.set_zlabel('Difference')
+# # ax.set_title('Deep Learning Solution')
+# plt.savefig('./MollData/Va_analysis_FB2.pdf', bbox_inches='tight')
+
+# fig = plt.figure(figsize=(16, 9))
+# # plt.plot(Z[:,0], Moll_Vacenter[:,0],label='$\partial_a v(a,z)$: Forward')
+# plt.plot(Z[:,0], Moll_Va[:,0],label=r'$\partial_a^{Upwind} v( \underline a,z)$', color='black')
+# # plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]), label=r'$u^\prime(z + r \underline{a})$')
+# plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
+#          label=r'$u^\prime(z + r \underline a )$', color='red')
+# # plt.view_init(35, 35)
+# plt.xlabel('$z$')
+# plt.ylim(0.75, 1.10)
+# plt.legend()
+# # ax.set_zlabel('$\partial V / \partial a$')
+# # ax.set_zlabel('Difference')
+# # ax.set_title('Deep Learning Solution')
+# plt.savefig('./MollData/Va_analysis_UB2.pdf', bbox_inches='tight')
+
 plt.style.use('classic')
 plt.rcParams["savefig.bbox"] = "tight"
 # plt.rcParams["figure.figsize"] = (10,8)
@@ -91,34 +135,15 @@ plt.rcParams['text.usetex']=True
 # plt.rcParams["lines.linewidth"] = 5
 
 fig = plt.figure(figsize=(16, 9))
-plt.plot(Z[:, 0], Moll_Vacenter[:, 0], label=r'$\partial_a^{Forward} v( \underline a,z)$', color='black')
-# plt.plot(Z[:,0], Moll_Va[:,0],label='$\partial_a v(a,z)$: Upwind')
-plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
-         label=r'$u^\prime(z + r \underline a )$', color='red')
-# plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
-#          label=r'$u^\prime(z + r a)$', color = 'red')
-# plt.view_init(35, 35)
-plt.xlabel('$z$')
-plt.ylim(0.75, 1.10)
-plt.legend()
-# plt.show()
-# ax.set_zlabel('$\partial V / \partial a$')
-# ax.set_zlabel('Difference')
-# ax.set_title('Deep Learning Solution')
-plt.savefig('./MollData/Va_analysis_FB2.pdf', bbox_inches='tight')
-
-fig = plt.figure(figsize=(16, 9))
 # plt.plot(Z[:,0], Moll_Vacenter[:,0],label='$\partial_a v(a,z)$: Forward')
-plt.plot(Z[:,0], Moll_Va[:,0],label=r'$\partial_a^{Upwind} v( \underline a,z)$', color='black')
-# plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]), label=r'$u^\prime(z + r \underline{a})$')
+plt.plot(Z[:, 0], Moll_Va_NN.reshape(n_plot, n_plot)[:, 0],
+         label=r'$\partial_a^{NN} v( \underline a,z)$', color='black')
+# plt.plot(Z[:,0], Moll_Va[:,0],label=r'$\partial_a^{Upwind} v( \underline a,z)$', color='black')
 plt.plot(Z[:, 0], u_deriv(Z[:, 0]+r*A[:, 0]),
          label=r'$u^\prime(z + r \underline a )$', color='red')
 # plt.view_init(35, 35)
 plt.xlabel('$z$')
-plt.ylim(0.75, 1.10)
+# plt.ylim(0.75, 1.10)
 plt.legend()
-# ax.set_zlabel('$\partial V / \partial a$')
-# ax.set_zlabel('Difference')
-# ax.set_title('Deep Learning Solution')
-plt.savefig('./MollData/Va_analysis_UB2.pdf', bbox_inches='tight')
 
+plt.savefig('./MollData/Va_slice.pdf', bbox_inches='tight')
